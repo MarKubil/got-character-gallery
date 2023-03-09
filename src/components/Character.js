044726752eba39b253756158cba4fd8ffed54086
MarKubil <marius.kub@outlook.com>
2023-03-09 20:51:05 +0000

    updated

--- src/components/Character.js
+++ src/components/Character.js
@@ -5,7 +5,7 @@ const Character = props => {
     return (
         <div data-test="component-character">
             <h1 data-test="char-name">{name}</h1>
-            <img alt="picture" data-test="char-img" src={imgUrl}/>
+            <img alt={name} data-test="char-img" src={imgUrl}/>
             <ul data-test="char-list">
                 <li data-test="char-birth">Date of Birth: {birth}</li>
                 <li data-test="char-death">Date of Death: {death}</li>
